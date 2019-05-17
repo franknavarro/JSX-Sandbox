42c56febf7eb8887d823788a75099459e7b84589
franknavarro <fnavarro7777@gmail.com>
2019-05-17 09:58:34 -0700

    Fixed attribute errors

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+  const buttonText = { text: "Click Me!" };
+
   return (
     <div>
-      <label class="class" for="name">Enter name:</label>
+      <label className="class" htmlFor="name">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
     </div>
   );
 };
